727a12b7f4b708852252c977e7ba4311071a5b7c
Elena <hellenatsvetkova@gmail.com>
2025-05-09 17:03:44 +0300

    feat: created 2 buckets for xml and pdf files in the importer

--- input/file_importer.py
+++ input/file_importer.py
@@ -2,88 +2,104 @@ import os
 
 from tkinterdnd2 import DND_FILES, TkinterDnD
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, Label
 
 
-class PdfWindowImporter:
+class FileImporter:
 
     def __init__(self):
         self.root = TkinterDnD.Tk()
-        self.root.title("PDF Importer")
+        self.root.title("File Importer")
         self.root.geometry("500x300")
 
-        self.file_path = None
+        self.pdf_path = None
+        self.xml_path = None
+
         self._create_widgets()
         self._configure_drag_and_drop()
 
     def _create_widgets(self):
-        self._create_drop_area()
-        self._create_file_list()
+        self._create_drop_areas()
         self._create_buttons()
 
-    def _create_drop_area(self):
-        self.drop_label = tk.Label(
+    def _create_drop_areas(self):
+        self.pdf_drop_label = self.__initialize_drop_label('PDF')
+        self.xml_drop_label = self.__initialize_drop_label('XML')
+
+    def __initialize_drop_label(self, type):
+        label = tk.Label(
             self.root,
-            text="Drag & Drop PDF files here\nor click 'Browse'",
+            text=f"Drag & Drop {type} files\nor click 'Browse'",
             bg="lightgray",
             relief=tk.RIDGE,
-            height=5
+            height=6
         )
-        self.drop_label.pack(fill=tk.BOTH, padx=10, pady=10, expand=True)
+        label.pack(fill=tk.X, padx=10, pady=10)
+        return label
 
     def _configure_drag_and_drop(self):
-        self.drop_label.drop_target_register(DND_FILES)
-        self.drop_label.dnd_bind("<<Drop>>", self._handle_drop)
 
-    def _create_file_list(self):
-        self.file_list = tk.Listbox(self.root, height=5)
-        self.file_list.pack(fill=tk.BOTH, padx=10, pady=(0, 10), expand=True)
+        for lbl, extension, file_type in (
+                (self.pdf_drop_label, '.pdf', 'pdf'),
+                (self.xml_drop_label, '.xml', 'xml')
+        ):
+            lbl.drop_target_register(DND_FILES)
+            lbl.dnd_bind("<<Drop>>", lambda e, x=extension, t=file_type: self.assign_file(self._extract_path(e), x, t))
 
     def _create_buttons(self):
         button_frame = tk.Frame(self.root)
         button_frame.pack(pady=5)
 
-        browse_btn = tk.Button(button_frame, text="Browse", command=self._handle_browse)
-        browse_btn.pack(side=tk.LEFT, padx=5)
+        buttons = {'Browse PDF': lambda: self._handle_browse("pdf", ".pdf", ("PDF files", "*.pdf")),
+                    'Browse XML': lambda: self._handle_browse("xml", ".xml", ("XML files", "*.xml")),
+                    'Import': self._handle_import}
 
-        import_btn = tk.Button(button_frame, text="Import", command=self._handle_import)
-        import_btn.pack(side=tk.LEFT, padx=5)
+        for btn_type, command in buttons.items():
+            tk.Button(button_frame, text=btn_type, command=command).pack(side=tk.LEFT, padx=5)
 
-    def _handle_drop(self, event):
-        # The event data returns a value like this -> {'C:\FILE_PATH'}
-        # so I need to extract only the clean path
-        raw_path = self.root.tk.splitlist(event.data)[0]
-        clean_path = raw_path.strip("{}'\"")
+    def _handle_drop(self, event, extension, file_type):
+        path = self._extract_path(event)
+        self.assign_file(path, extension, file_type)
 
-        if self.__is_valid_pdf(clean_path):
-            self.file_path = clean_path
-            self._update_listbox()
-        else:
-            print(f"Invalid PDF file: {clean_path}")
-
-    def _handle_browse(self):
-        path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
-        if self.__is_valid_pdf(path):
-            self.file_path = path
-            self._update_listbox()
+    def _handle_browse(self, file_type, extension, allowed_files):
+        path = filedialog.askopenfilename(filetypes=[allowed_files])
+        self.assign_file(path, extension, file_type)
 
     def _handle_import(self):
-        if self.file_path is not None:
+        if self.xml_path and self.pdf_path:
             self.root.destroy()  # If the import is confirmed, the window closes
 
-    @staticmethod
-    def __is_valid_pdf(path):
-        return path.lower().endswith(".pdf") and os.path.exists(path)
+    def assign_file(self, path, extension, file_type):
 
-    def _update_listbox(self):
-        """ Updates the Listbox to display the selected file. """
-        self.file_list.delete(0, tk.END)
-        self.file_list.insert(tk.END, self.file_path)
+        if self.__is_valid_file(path, extension):
+            name = os.path.basename(path)
+            if file_type == "pdf":
+                self.pdf_path = path
+                self.pdf_drop_label.config(text=name)
+            elif file_type == "xml":
+                self.xml_path = path
+                self.xml_drop_label.config(text=name)
+
+    @staticmethod
+    def __is_valid_file(path, extension):
+        return path.lower().endswith(extension) and os.path.exists(path)
+
+    def _extract_path(self, event):
+        raw_path = self.root.tk.splitlist(event.data)[0]
+        return raw_path.strip("{}'\"")
+
+    # def _update_listbox(self):
+    #     """ Updates the Listbox to display the selected file. """
+    #     self.file_list.delete(0, tk.END)
+    #     if self.pdf_path:
+    #         self.file_list.insert(tk.END, f"PDF: {self.pdf_path}")
+    #     if self.xml_path:
+    #         self.file_list.insert(tk.END, f"XML: {self.xml_path}")
 
     def import_file(self):
         self.root.mainloop()
-        return self.file_path
+        return {"pdf": self.pdf_path, "xml": self.xml_path}
 
     @property
     def file_name(self):
-        return os.path.basename(self.file_path)
+        return os.path.basename(self.pdf_path)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from file_manager import FileManager
-from input.file_importer import PdfWindowImporter
+from input.file_importer import FileImporter
 from output.file_exporter import ExcelExporter
 
 
@@ -7,14 +7,14 @@ class InvoiceDataCreator:
 
     @staticmethod
     def run():
-        pdf_importer = PdfWindowImporter()
+        pdf_importer = FileImporter()
         file_path = pdf_importer.import_file()
 
         file_manager = FileManager(file_path)
         file_manager.create_import_data()
 
-        exporter = ExcelExporter()
-        exporter.export_to_excel()
+        # exporter = ExcelExporter()
+        # exporter.export_to_excel()
 
 if __name__ == "__main__":
     InvoiceDataCreator().run()
